1bb8c7f4873ebb434cdf02fb792f917bc4ab50cd
svsm03 <svsm03@hotmail.com>
2023-11-09 16:33:13 -0600

    Conexion con Firenase y Plant toggle menu

--- App.js
+++ App.js
@@ -1,52 +1,88 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, TouchableWithoutFeedback } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome'; // Puedes cambiar el ícono según tus preferencias
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Image} from 'react-native';
+import ModalDropdown from 'react-native-modal-dropdown';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, onValue } from 'firebase/database';
 
 export default function App() {
   const [isMenuOpen, setMenuOpen] = useState(false);
+  const [distancia, setDistancia] = useState(0);
+  const [movimiento, setMovimiento] = useState(0);
+  const [temperatura, setTemperatura] = useState(0);
+  const [selectedPlant, setSelectedPlant] = useState('Plant1'); // Elige un valor predeterminado
 
-  const toggleMenu = () => {
-    setMenuOpen(!isMenuOpen);
+
+  // Configura la conexión a Firebase y escucha cambios en tiempo real
+  const firebaseConfig = {
+    apiKey: "AIzaSyAt5_BrZyNPK2hoLvBXMDjeyAY9pOmNqsY",
+    authDomain: "gardensense-cfe37.firebaseapp.com",
+    databaseURL: "https://gardensense-cfe37-default-rtdb.firebaseio.com",
+    projectId: "gardensense-cfe37",
+    storageBucket: "gardensense-cfe37.appspot.com",
+    messagingSenderId: "949510113189",
+    appId: "1:949510113189:web:94c542b2d64df8fc2c4a4c",
+    measurementId: "G-0TR1T5V5ZP"
   };
+
+  const firebaseApp = initializeApp(firebaseConfig);
+  const db = getDatabase(firebaseApp);
+  const sensoresRef = ref(db, 'sensores');
+
+  useEffect(() => {
+    onValue(sensoresRef, (snapshot) => {
+      const data = snapshot.val();
+      setDistancia(data.distancia);
+      setMovimiento(data.movimiento);
+      setTemperatura(data.temperatura);
+    });
+  }, []);
+
+  const plants = ['Plant1', 'Plant2', 'Plant3', 'Plant4', 'Plant5'];
 
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <TouchableWithoutFeedback onPress={toggleMenu}>
-          <View style={styles.menuIcon}>
-            <Icon name={isMenuOpen ? 'close' : 'bars'} size={30} color="#fff" />
-          </View>
-        </TouchableWithoutFeedback>
-        <Text style={styles.headerText}>Home</Text>
+  <View style={styles.header}>
+    <Text style={styles.headerText}>Home</Text>
+  </View>
+
+  <View style={styles.content}>
+  <View style={styles.plantInfoContainer}>
+      <View style={styles.menuIconContainer}>
+        <ModalDropdown
+          options={plants}
+          onSelect={(index, value) => setSelectedPlant(value)}
+          style={styles.menuIcon}
+        >
+          <Image source={require('./images/toggle.png')} style={styles.dropdownOptionImage} />
+        </ModalDropdown>
       </View>
-
-      {isMenuOpen && (
-        // Agregar aquí el contenido del menú que se mostrará al hacer clic en el ícono de menú
-        <View style={styles.menu}>
-          {/* Puedes agregar elementos de menú aquí */}
-        </View>
-      )}
-
-      <View style={styles.content}>
-        <View style={styles.rectangleContainer}>
-          <View style={styles.line}>
-            <View style={styles.lineBorder}></View>
-          </View>
-
-          <View style={styles.rectangle}>
-            <Text style={styles.rectangleText}>34.5</Text>
-          </View>
-
-          <View style={styles.rectangle}>
-            <Text style={styles.rectangleText}>34.5</Text>
-          </View>
-
-          <View style={styles.rectangle}>
-            <Text style={styles.rectangleText}>34.5</Text>
-          </View>
-        </View>
+      <View style={styles.plantName}>
+        <Text style={styles.headerText}>{selectedPlant}</Text>
       </View>
     </View>
+
+    <View style={styles.rectangleContainer}>
+      <View style={styles.line}>
+        <View style={styles.lineBorder}></View>
+      </View>
+
+      <View style={styles.rectangle}>
+        <Text style={styles.rectangleText}>{distancia}</Text>
+      </View>
+
+      <View style={styles.rectangle}>
+        <Text style={[styles.rectangleText, movimiento === 1 ? { color: 'red' } : null]}>
+          {movimiento === 0 ? 'A salvo' : '¡Cuidado!'}
+        </Text>
+      </View>
+
+      <View style={styles.rectangle}>
+        <Text style={styles.rectangleText}>{temperatura}</Text>
+      </View>
+    </View>
+  </View>
+</View>
+
   );
 }
 
@@ -68,11 +104,11 @@ const styles = StyleSheet.create({
     fontFamily: 'Quicksand',
     marginLeft: 10, // Ajusta el margen según tus necesidades
   },
-  menuIcon: {
-    marginLeft: 10,
-  },
-  menu: {
-    // Estilos del menú, puedes ajustarlos según tus necesidades
+  pickerContainer: {
+    top: 0,
+    left: 0,
+    right: 0,
+    zIndex: 1,
   },
   content: {
     flex: 1,
@@ -106,5 +142,18 @@ const styles = StyleSheet.create({
     height: 2,
     backgroundColor: 'black',
   },
-  // Otros estilos...
+  plantInfoContainer: {
+    flexDirection: 'row', // Esto establece la dirección de fila
+    alignItems: 'center', // Esto alinea los elementos verticalmente en el centro
+  },
+  
+  // Estilos para el nombre de la planta
+  plantName: {
+    flex: 1,
+  },
+
+  // Estilos para el contenedor del menú del picker
+  menuIconContainer: {
+    paddingLeft: 10,
+  },
 });
