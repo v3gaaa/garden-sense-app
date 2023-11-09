e04fd207795371064eab0bf71f483538cff1ec91
v3gaaa <svsm03@hotmail.com>
2023-11-09 14:10:45 -0600

    HomePage initial updates

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableWithoutFeedback } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome'; // Puedes cambiar el ícono según tus preferencias
 
 export default function App() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableWithoutFeedback onPress={toggleMenu}>
+          <View style={styles.menuIcon}>
+            <Icon name={isMenuOpen ? 'close' : 'bars'} size={30} color="#fff" />
+          </View>
+        </TouchableWithoutFeedback>
+        <Text style={styles.headerText}>Home</Text>
+      </View>
+
+      {isMenuOpen && (
+        // Agregar aquí el contenido del menú que se mostrará al hacer clic en el ícono de menú
+        <View style={styles.menu}>
+          {/* Puedes agregar elementos de menú aquí */}
+        </View>
+      )}
+
+      <View style={styles.content}>
+        <View style={styles.rectangleContainer}>
+          <View style={styles.line}>
+            <View style={styles.lineBorder}></View>
+          </View>
+
+          <View style={styles.rectangle}>
+            <Text style={styles.rectangleText}>34.5</Text>
+          </View>
+
+          <View style={styles.rectangle}>
+            <Text style={styles.rectangleText}>34.5</Text>
+          </View>
+
+          <View style={styles.rectangle}>
+            <Text style={styles.rectangleText}>34.5</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +53,58 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E4E4D0',
   },
+  header: {
+    height: 90,
+    backgroundColor: '#94A684',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#fff',
+    fontFamily: 'Quicksand',
+    marginLeft: 10, // Ajusta el margen según tus necesidades
+  },
+  menuIcon: {
+    marginLeft: 10,
+  },
+  menu: {
+    // Estilos del menú, puedes ajustarlos según tus necesidades
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  rectangleContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  rectangle: {
+    width: 300,
+    height: 100,
+    backgroundColor: '#B3A492',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+    borderRadius: 10,
+  },
+  rectangleText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  line: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+  },
+  lineBorder: {
+    flex: 1,
+    height: 2,
+    backgroundColor: 'black',
+  },
+  // Otros estilos...
 });
---
+++ README.md
@@ -0,0 +1,12 @@
+# Realtime Database integration with flutter and firebase
+
+A practice for my IOT course where I had to send the data from several sensors through my esp32
+to firebase and save it in a Realtime Database to later access it through flutter and display it in a graphical interface.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
